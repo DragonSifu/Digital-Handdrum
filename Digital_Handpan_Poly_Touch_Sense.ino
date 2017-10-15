f0c0a0f1da42623111382799bb1cab115e5b91ea
DragonSifu <cytronimei@yahoo.de>
2017-10-15 18:41:58 +0900

    Add files via upload

---
+++ Digital_Handpan_Poly_Touch_Sense.ino
@@ -0,0 +1,290 @@
+/******************************************************************************
+  Digital Handpan Code for Teesny 
+  Joel Bartlett @ SparkFun Electronics
+  Original Creation Date: October 16, 2016
+  
+  This sketch produces polyphonic notes when any number of eight capacitive touch pads are 
+  touched, thus replicating a Handpan (or Hang) steel drum. This sketch 
+  makes use of the Teensy Audio Shield (https://www.sparkfun.com/products/12767)
+  as well as the Teensy Audio System Design Tool (http://www.pjrc.com/teensy/gui/).
+  All capactive touch sensing is handled by the Teensy's built in Touch Sense Pins. 
+  
+  Hardware:
+    Teensy 3.2 Dev Board (https://www.sparkfun.com/products/13736)
+    Teensy Audio Board (https://www.sparkfun.com/products/12767)
+    Various buttons and potentiometers 
+
+  Development environment specifics:
+    IDE: Arduino IDE V 1.6.11 with the Teensyduino Add-on installed V Teensy Loader V1.30
+
+  This code is beerware; if you see me (or any other SparkFun
+  employee) at the local, and you've found our code helpful,
+  please buy us a round!
+  Distributed as-is; no warranty is given.
+*******************************************************************************/
+//These libraries are included by the Teensy Audio System Design Tool 
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+//#include <SD.h>//not using the SD library at this time
+#include <SerialFlash.h>
+#include <Bounce.h>
+
+#include "note_frequency.h"
+#include "scales.h"
+AudioEffectDelayExternal delayExt1(AUDIO_MEMORY_CY15B104,6000);      //xy=1092,172
+
+#include "TeensyAudioDesignTool.h"
+#define SDCARD_MOSI_PIN  7
+#define SDCARD_SCK_PIN   14
+
+//an array to define each of the eight touch sense pins, 
+//all other touch sense pins are used by the Teensy Audio Shield (16,17,18,19)
+int pinTouch[] = {33,32,25,17,16,15,1,0};
+
+int scale_index = 0;//var to keep track fo which scale is being used
+#define FLANGE_DELAY_LENGTH (5*AUDIO_BLOCK_SAMPLES)
+// Allocate the delay lines for left and right channels
+short l_delayline[FLANGE_DELAY_LENGTH];
+int s_idx = FLANGE_DELAY_LENGTH/4;
+int s_depth = FLANGE_DELAY_LENGTH/4;
+double s_freq = .25;
+
+int n_chorus = 3;
+
+int dcVal = 0;//value to control the decay of each note 
+int fbval = 0;//value to control the decay of each note 
+int shift = 0;//value to control the decay of each note 
+float fBack = 1;//value to control the decay of each note 
+
+//buttons for incrementing or decrementing through each scale
+Bounce button0 = Bounce(2, 15);
+Bounce button1 = Bounce(3, 15);    
+
+bool debug =1;//Set to 1 for Serial debugging 
+
+////////////////////////////////////////////////////////
+void setup() 
+{
+  //initialize buttons
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  button0.update();
+  button1.update();
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(24, OUTPUT);
+//  digitalWrite(4, HIGH);
+//  digitalWrite(5, HIGH);
+//  digitalWrite(8, HIGH);
+  //initialize Serial
+  Serial.begin(115200);
+  flange1.begin(l_delayline,FLANGE_DELAY_LENGTH,s_idx,s_depth,s_freq);
+  flange1.voices(s_idx,s_depth,s_freq);
+
+  //set aside audio memory 
+  AudioMemory(128);
+        SPI.setMOSI(SDCARD_MOSI_PIN);
+        SPI.setSCK(SDCARD_SCK_PIN);
+
+  //initialize audio settings
+
+  //initialize volume
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(0.8);
+  //enable line out for troubleshooting on O-scope
+  //sgtl5000_1.unmuteLineout();
+  filter1.resonance(2);
+  filter2.resonance(2);
+  filter3.resonance(2);
+  filter4.resonance(2);
+  filter1.frequency(110);
+  filter2.frequency(220);
+  filter3.frequency(440);
+  filter4.frequency(880);
+  filter1.octaveControl(2);
+  filter2.octaveControl(2);
+  filter3.octaveControl(2);
+  filter4.octaveControl(2);
+// biquad1.setLowpass(0,440,0.7);
+//  biquad1.setLowpass(1,660,0.7);
+//  biquad1.setLowpass(2,880,0.7);
+//  biquad1.setLowpass(3,1000,0.7);
+   delayExt1.delay(5,20);
+   delayExt1.delay(4,100);
+   delayExt1.delay(3,400);
+   delayExt1.delay(2,1000);
+   delayExt1.delay(1,4000);
+   delayExt1.delay(0,6000);
+   sine9.phase(90);
+  sine10.phase(90); 
+  sine11.phase(90);
+  sine12.phase(90); 
+  sine13.phase(90);  
+  sine14.phase(90); 
+  sine15.phase(90);  
+  sine16.phase(90);  
+}
+//////////////////////////////////////////////
+void loop() 
+{     
+
+  volumeCheck();//check the volume knob 
+
+  touchCheck();//check if any of the capacitive pads have been touched
+
+  dcValCheck();//check the decay knob
+
+  buttonCheck();//check for button presses to change the scale 
+  filter1.frequency(2*note_frequency[scale[scale_index][1]]);
+  filter2.frequency(2*note_frequency[scale[scale_index][3]]);
+  filter3.frequency(2*note_frequency[scale[scale_index][5]]);
+  filter4.frequency(2*note_frequency[scale[scale_index][7]]);
+
+}
+/////////////////////////////////////////////////////
+void volumeCheck()
+{
+  //check knob value for volume 
+  int knob = analogRead(A7);
+  int fknob = analogRead(A17);
+  shift= map(analogRead(A16), 0, 1023,0, 12);
+  if(shift-6==0)
+    digitalWrite(24, HIGH );
+  else
+    digitalWrite(24, LOW);
+  
+  float vol = (float)knob / 1280.0;
+  fBack = (float)fknob / 640.0;
+  sgtl5000_1.volume(vol);
+   if(debug == 1)
+  {  
+  Serial.print("A17 = ");  
+  Serial.println(fBack);  
+  Serial.print("A16 = ");  
+  Serial.println(shift);  
+  }
+}
+/////////////////////////////////////////////////////
+void dcValCheck()
+{
+  //check knob and set value as delay on dc constant for sine wave decay
+  dcVal = map(analogRead(A6), 0, 1023, 1, 1000);
+  fbval = map(analogRead(A15), 0, 1023,0, 21);
+  float f=1;
+  if(fbval >6)
+     f=0.5;
+  if(fbval >22)
+     f=1.0/3.0;
+  if(fbval >41)
+     f=1.0/4.0;
+  if(fbval >56)
+     f=1.0/5.0;
+  if(fbval >62)
+     f=1.0/6.0;
+  int *mix=heads[fbval];
+  f=f*fBack;
+    mixer5.gain(1,mix[0]*f);
+    mixer5.gain(2,mix[1]*f);
+    mixer6.gain(0,mix[2]*f);
+    mixer6.gain(1,mix[3]*f);
+    mixer6.gain(2,mix[4]*f);
+    mixer6.gain(3,mix[5]*f);
+  
+  if(debug == 1)
+  {  
+  Serial.print("A15 = ");  
+  Serial.println(fbval);
+  Serial.print("f = ");  
+  Serial.print(mix[0]);  
+  Serial.print("  ");  
+  Serial.print(mix[5]);  
+  Serial.print("  ");  
+ Serial.println(f);
+  }
+
+}
+/////////////////////////////////////////////////////
+void hang(int t,AudioSynthWaveformSine *sine,AudioSynthWaveformSine *sinex,AudioSynthWaveformDc *dc,AudioSynthWaveformDc *dcx)
+{
+  int touch =touchRead(pinTouch[t]);
+         if (touch > 2000)
+        {
+          //once a pad is touched, a value from the note frquency froma table is looked up via a 2D table
+          //with x corresponding to a scale and y corresponding to one of the eight notes on the drum. 
+         int ps=shift-6;
+          sine->frequency(note_frequency[scale[scale_index][t]+ps]);
+          dc->amplitude(0.8, 5);
+          sinex->frequency(2*note_frequency[scale[scale_index][t]+ps]);
+          dcx->amplitude(0.5, 6);
+      }
+      if (touch <= 2000) 
+      {
+        //one the pad is released, the note fades out with a decay val set by the dcVal knob
+        dc->amplitude(0, dcVal);
+        dcx->amplitude(0, dcVal*1.2);
+     }
+
+}
+/////////////////////////////////////////////////////
+void touchCheck()
+{
+  //Each capacitive touch pad will vary based on the size and material it is made of
+  //The value necessary to trigger each note will require some trial and error to get the
+  //sensitivity just right. Try adjusting these values to get the best response. 
+  int t=0;
+  hang(t++,&sine1,&sine2,&dc1,&dc2);  
+  hang(t++,&sine3,&sine4,&dc3,&dc4);  
+  hang(t++,&sine5,&sine6,&dc5,&dc6);  
+  hang(t++,&sine7,&sine8,&dc7,&dc8);  
+  hang(t++,&sine9,&sine10,&dc9,&dc10);  
+  hang(t++,&sine11,&sine12,&dc11,&dc12);  
+  hang(t++,&sine13,&sine14,&dc13,&dc14);  
+  hang(t++,&sine15,&sine16,&dc15,&dc16);  
+}
+//////////////////////////////////////////////////
+void buttonCheck()
+{
+  button0.update();
+  button1.update();
+
+  //if button 0 is pressed, increment the scale being used
+  if (button0.risingEdge())
+  { 
+    scale_index++;
+    //check for overflow
+    if(scale_index > numOfScales)//numOfScales variable found in the scales.h file
+    scale_index = 0;
+    }
+
+  //if button 1 is pressed, decrement the scale being used
+   if (button1.fallingEdge())
+      flange1.voices(0,0,0);
+  if (button1.risingEdge())
+  { 
+    flange1.voices(s_idx,s_depth,s_freq);
+    scale_index--;
+    //check for negative numbers
+    if(scale_index < 0)
+    scale_index = numOfScales;//numOfScales variable found in the scales.h file
+  }
+   if((scale_index+1)%2)
+       digitalWrite(4, HIGH);
+    if((scale_index+1)%2==0)
+       digitalWrite(4, LOW);
+  if(((scale_index+1)/2)%2)
+       digitalWrite(5, HIGH);
+    if(((scale_index+1)/2)%2==0)
+       digitalWrite(5, LOW);
+   if(((scale_index+1)/4)%2)
+       digitalWrite(8, HIGH);
+    if(((scale_index+1)/4)%2==0)
+       digitalWrite(8, LOW);
+
+  if(debug == 0)
+  {  
+  Serial.print("Scale = ");  
+  Serial.println(scale_index);
+  }
+}
---
+++ TeensyAudioDesignTool.h
@@ -0,0 +1,141 @@
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioSynthWaveformSine   sine1;          //xy=88,29
+AudioSynthWaveformSine   sine2;          //xy=88,87
+AudioSynthWaveformSine   sine3;          //xy=88,145
+AudioSynthWaveformSine   sine4;          //xy=88,203
+AudioSynthWaveformSine   sine9;          //xy=88,261
+AudioSynthWaveformSine   sine10;         //xy=88,319
+AudioSynthWaveformSine   sine12;         //xy=88,377
+AudioSynthWaveformSine   sine11;         //xy=88,435
+AudioSynthWaveformSine   sine13;         //xy=88,493
+AudioSynthWaveformSine   sine14;         //xy=88,551
+AudioSynthWaveformSine   sine15;         //xy=88,609
+AudioSynthWaveformSine   sine16;         //xy=88,667
+AudioSynthWaveformSine   sine5;          //xy=88,725
+AudioSynthWaveformSine   sine6;          //xy=88,783
+AudioSynthWaveformSine   sine7;          //xy=88,841
+AudioSynthWaveformSine   sine8;          //xy=88,899
+AudioSynthWaveformDc     dc1;            //xy=264,42
+AudioSynthWaveformDc     dc2;            //xy=264,100
+AudioSynthWaveformDc     dc3;            //xy=264,158
+AudioSynthWaveformDc     dc4;            //xy=264,216
+AudioSynthWaveformDc     dc9;            //xy=264,274
+AudioSynthWaveformDc     dc10;           //xy=264,332
+AudioSynthWaveformDc     dc12;           //xy=264,390
+AudioSynthWaveformDc     dc11;           //xy=264,448
+AudioSynthWaveformDc     dc13;           //xy=264,506
+AudioSynthWaveformDc     dc14;           //xy=264,564
+AudioSynthWaveformDc     dc15;           //xy=264,622
+AudioSynthWaveformDc     dc16;           //xy=264,680
+AudioSynthWaveformDc     dc5;            //xy=264,738
+AudioSynthWaveformDc     dc6;            //xy=264,796
+AudioSynthWaveformDc     dc7;            //xy=264,854
+AudioSynthWaveformDc     dc8;            //xy=264,912
+AudioEffectMultiply      multiply1;      //xy=464,36
+AudioEffectMultiply      multiply2;      //xy=464,94
+AudioEffectMultiply      multiply3;      //xy=464,152
+AudioEffectMultiply      multiply4;      //xy=464,210
+AudioEffectMultiply      multiply9;      //xy=464,268
+AudioEffectMultiply      multiply10;     //xy=464,326
+AudioEffectMultiply      multiply12;     //xy=464,384
+AudioEffectMultiply      multiply11;     //xy=464,442
+AudioEffectMultiply      multiply13;     //xy=464,500
+AudioEffectMultiply      multiply14;     //xy=464,558
+AudioEffectMultiply      multiply15;     //xy=464,616
+AudioEffectMultiply      multiply16;     //xy=464,674
+AudioEffectMultiply      multiply5;      //xy=464,732
+AudioEffectMultiply      multiply6;      //xy=464,790
+AudioEffectMultiply      multiply7;      //xy=464,848
+AudioEffectMultiply      multiply8;      //xy=464,906
+AudioMixer4              mixer4;         //xy=703,575
+AudioMixer4              mixer2;         //xy=703,807
+AudioMixer4              mixer3;         //xy=707,348
+AudioMixer4              mixer1;         //xy=710,122
+AudioFilterStateVariable filter1;        //xy=857,128
+AudioFilterStateVariable filter3;        //xy=876,582
+AudioFilterStateVariable filter2;        //xy=878,355
+AudioFilterStateVariable filter4;        //xy=882,813
+AudioMixer4              mixerMain;      //xy=1070,332
+AudioSynthWaveformDc     dc17;           //xy=1080,465
+AudioEffectFlange        flange1;        //xy=1272,335
+//AudioEffectDelayExternal delayExt1;      //xy=1273,211
+AudioMixer4              mixer6;         //xy=1532,185
+AudioMixer4              mixer5;         //xy=1533,283
+AudioOutputI2S           i2s1;           //xy=1659,491
+AudioConnection          patchCord1(sine1, 0, multiply1, 0);
+AudioConnection          patchCord2(sine2, 0, multiply2, 0);
+AudioConnection          patchCord3(sine3, 0, multiply3, 0);
+AudioConnection          patchCord4(sine4, 0, multiply4, 0);
+AudioConnection          patchCord5(sine9, 0, multiply9, 0);
+AudioConnection          patchCord6(sine10, 0, multiply10, 0);
+AudioConnection          patchCord7(sine12, 0, multiply12, 0);
+AudioConnection          patchCord8(sine11, 0, multiply11, 0);
+AudioConnection          patchCord9(sine13, 0, multiply13, 0);
+AudioConnection          patchCord10(sine14, 0, multiply14, 0);
+AudioConnection          patchCord11(sine15, 0, multiply15, 0);
+AudioConnection          patchCord12(sine16, 0, multiply16, 0);
+AudioConnection          patchCord13(sine5, 0, multiply5, 0);
+AudioConnection          patchCord14(sine6, 0, multiply6, 0);
+AudioConnection          patchCord15(sine7, 0, multiply7, 0);
+AudioConnection          patchCord16(sine8, 0, multiply8, 0);
+AudioConnection          patchCord17(dc1, 0, multiply1, 1);
+AudioConnection          patchCord18(dc2, 0, multiply2, 1);
+AudioConnection          patchCord19(dc3, 0, multiply3, 1);
+AudioConnection          patchCord20(dc4, 0, multiply4, 1);
+AudioConnection          patchCord21(dc9, 0, multiply9, 1);
+AudioConnection          patchCord22(dc10, 0, multiply10, 1);
+AudioConnection          patchCord23(dc12, 0, multiply12, 1);
+AudioConnection          patchCord24(dc11, 0, multiply11, 1);
+AudioConnection          patchCord25(dc13, 0, multiply13, 1);
+AudioConnection          patchCord26(dc14, 0, multiply14, 1);
+AudioConnection          patchCord27(dc15, 0, multiply15, 1);
+AudioConnection          patchCord28(dc16, 0, multiply16, 1);
+AudioConnection          patchCord29(dc5, 0, multiply5, 1);
+AudioConnection          patchCord30(dc6, 0, multiply6, 1);
+AudioConnection          patchCord31(dc7, 0, multiply7, 1);
+AudioConnection          patchCord32(dc8, 0, multiply8, 1);
+AudioConnection          patchCord33(multiply1, 0, mixer1, 0);
+AudioConnection          patchCord34(multiply2, 0, mixer1, 1);
+AudioConnection          patchCord35(multiply3, 0, mixer1, 2);
+AudioConnection          patchCord36(multiply4, 0, mixer1, 3);
+AudioConnection          patchCord37(multiply9, 0, mixer3, 0);
+AudioConnection          patchCord38(multiply10, 0, mixer3, 1);
+AudioConnection          patchCord39(multiply12, 0, mixer3, 2);
+AudioConnection          patchCord40(multiply11, 0, mixer3, 3);
+AudioConnection          patchCord41(multiply13, 0, mixer4, 0);
+AudioConnection          patchCord42(multiply14, 0, mixer4, 1);
+AudioConnection          patchCord43(multiply15, 0, mixer4, 2);
+AudioConnection          patchCord44(multiply16, 0, mixer4, 3);
+AudioConnection          patchCord45(multiply5, 0, mixer2, 0);
+AudioConnection          patchCord46(multiply6, 0, mixer2, 1);
+AudioConnection          patchCord47(multiply7, 0, mixer2, 2);
+AudioConnection          patchCord48(multiply8, 0, mixer2, 3);
+AudioConnection          patchCord49(mixer4, 0, filter3, 0);
+AudioConnection          patchCord50(mixer2, 0, filter4, 0);
+AudioConnection          patchCord51(mixer3, 0, filter2, 0);
+AudioConnection          patchCord52(mixer1, 0, filter1, 0);
+AudioConnection          patchCord53(filter1, 0, mixerMain, 0);
+AudioConnection          patchCord54(filter3, 0, mixerMain, 2);
+AudioConnection          patchCord55(filter2, 0, mixerMain, 1);
+AudioConnection          patchCord56(filter4, 0, mixerMain, 3);
+AudioConnection          patchCord57(mixerMain, flange1);
+AudioConnection          patchCord58(flange1, 0, mixer5, 3);
+AudioConnection          patchCord59(delayExt1, 0, mixer6, 0);
+AudioConnection          patchCord60(delayExt1, 1, mixer6, 1);
+AudioConnection          patchCord61(delayExt1, 2, mixer6, 2);
+AudioConnection          patchCord62(delayExt1, 3, mixer6, 3);
+AudioConnection          patchCord63(delayExt1, 4, mixer5, 1);
+AudioConnection          patchCord64(delayExt1, 5, mixer5, 2);
+AudioConnection          patchCord65(mixer6, 0, mixer5, 0);
+AudioConnection          patchCord66(mixer5, delayExt1);
+AudioConnection          patchCord67(mixer5, 0, i2s1, 0);
+AudioConnection          patchCord68(mixer5, 0, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=1496,578
+// GUItool: end automatically generated code
+
